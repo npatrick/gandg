a41914d2d3fc2254840698fd52dfb8eae1a2f7a1
npatrick <wazzupnickz@gmail.com>
2018-11-07 18:00:55 -0800

    add basic changes

--- src/server/server.js
+++ src/server/server.js
@@ -7,4 +7,18 @@ const path = require('path');
 
 const app = express();
 
+// http logging
+app.use(morgan('tiny', {
+	skip: function (req, res) {
+		return res.statusCode < 400
+	}, stream: process.stderr
+}));
+app.use(morgan('tiny', {
+	skip: function (req, res) {
+		return res.statusCode >= 400
+	}, stream: process.stdout
+}));
+
+app.use('/', express.static(path.join(__dirname, '../client')));
+
 module.exports = app;
