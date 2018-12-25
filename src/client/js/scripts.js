32019e260ed2c58a3f5ae360cca69785fd17d4cb
Neil Patrick Romana <wazzupnickz@gmail.com>
2018-12-24 16:06:31 -0800

    add email handler on frontend

--- src/client/js/scripts.js
+++ src/client/js/scripts.js
@@ -8,6 +8,14 @@
  * 5. Portfolio
  */
 
+var apiHome = 'https://gandgconstruction.herokuapp.com';
+// Ping Node Server
+$.get(apiHome)
+	.done(function (data) {
+	})
+	.fail(function (err) {
+	});
+
 (function ($) {
 	// filtering & sorting transitions
 	$.fn.hukaIsotope = function (opts) {
@@ -326,11 +334,95 @@
 			$projectInner.scrollToFixed(options);
 		}
 
-		// Contact Form
+		// ================== Contact Form ==================== //
+		
 		// Add structure to phone number like parentheses & hypen
 		if ($('.form-phone > input').length) {
 			$('.form-phone > input').mask('(000) 000-0000');
 		}
+		// Email address validation
+		function isValidEmailAddress(emailAddress) {
+			var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+			return pattern.test(emailAddress);
+		};
+		// Email Submission
+		$('#contact-form').submit(function (e) {
+			e.preventDefault();
+
+			var $formContext = $(this);
+			var message = $('.form-message > textarea').val();
+			var name =  $(this).find('.form-name > input').val();
+			var email =  $(this).find('.form-email > input').val();
+			var phone =  $(this).find('.form-phone > input').val();
+			var responseMessage = $('#submit');
+
+			if ((name == '' || email == '' || phone == '') || (!isValidEmailAddress(email))) {
+				responseMessage.css({
+					'font-size': '14px',
+					'background-color': '#a53232'
+				});
+				responseMessage.attr('value', 'Please fix the errors and try again.');
+				return;
+			} 
+			if (message.length < 1) {
+				if (referredFrom.length < 1) {
+					responseMessage.css({
+						'font-size': '14px',
+						'background-color': '#a53232'
+					});
+					responseMessage.attr('value', 'Please fix the errors and try again.');
+					return
+				}
+			}
+			console.log('will be submitting with:\n', 
+				message, '\n',
+				name, '\n',
+				email, '\n',
+				phone);
+			$.post({
+				url: apiHome + '/sendemail',
+				data: {
+					message: message,
+					name: name,
+					email: email,
+					phone: phone
+				},
+				beforeSend: function(result) {
+					$formContext.find('.form-submit input').empty();
+					$formContext.find('.form-submit input').val('Wait...');
+				}
+			})
+				.done(function (result) {
+					if (result !== 'Message sent!') {
+						var serverResponse;
+						if (result.length < 100) {
+							serverResponse = result;
+						} else {
+							serverResponse = 'Oops, something went wrong on our web';
+						}
+						$formContext.find('.form-submit input').val(serverResponse);
+						$formContext.find('.form-submit input').css({
+							'font-size': '14px',
+							'background-color': '#a53232'
+						});
+						return;
+					}
+					$formContext.trigger('reset');
+					$formContext.find('.form-submit input').empty();
+					$formContext.find('.form-submit input').val('Sent!');
+					$formContext.find('.ajax-hidden').fadeOut(1800);
+				})
+				.fail(function (err) {
+					$formContext.find('.form-submit input').empty();
+					$formContext.find('.form-submit input').val('Oops, something went wrong from our side.');
+					$formContext.find('.form-submit input').css({
+						'font-size': '14px',
+						'background-color': '#a53232'
+					});
+					responseMessage.html(err.responseText).fadeIn(1000);
+				});
+			return false;
+		});
 
 
 		$(window).on('load', function () {
